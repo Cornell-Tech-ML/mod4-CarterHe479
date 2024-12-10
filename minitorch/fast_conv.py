0abd88f9c1ba1a820066002d9b80364cd4380332
Carter He <carterhes479@gmail.com>
2024-12-10 23:52:30 +0800

    Final Fix

--- minitorch/fast_conv.py
+++ minitorch/fast_conv.py
@@ -1,49 +1,45 @@
 from typing import Tuple, TypeVar, Any
 import numpy as np
-from numba import prange, njit as _njit
+from numba import njit as _njit
+from numba import prange
 
 from .autodiff import Context
 from .tensor import Tensor
 from .tensor_data import (
-    Index,
     Shape,
     Strides,
+    Storage,
     broadcast_index,
     index_to_position,
     to_index,
+    Index,
 )
 from .tensor_functions import Function
 
 Fn = TypeVar("Fn")
 
 
-def njit(fn: Fn = None, **kwargs: Any) -> Fn:  # noqa: D103
-    def decorator(inner_fn: Fn) -> Fn:
-        return _njit(inner_fn, inline="always", **kwargs)  # type: ignore
-
-    # If used directly (e.g., njit(my_function)), call the decorator
-    if fn is not None:
-        return decorator(fn)  # type: ignore
-
-    # If used as a decorator (@njit), return the decorator itself
-    return decorator  # type: ignore
+def njit(fn: Fn, **kwargs: Any) -> Fn:  # noqa: D103
+    return _njit(inline="always", **kwargs)(fn)  # type: ignore
 
 
-# JIT-compiled tensor_data functions
+# This code will JIT compile fast versions your tensor_data functions.
+# If you get an error, read the docs for NUMBA as to what is allowed
+# in these functions.
 to_index = njit(to_index)
 index_to_position = njit(index_to_position)
 broadcast_index = njit(broadcast_index)
 
 
 def _tensor_conv1d(
-    out: Tensor,
+    out: Storage,
     out_shape: Shape,
     out_strides: Strides,
     out_size: int,
-    input: Tensor,
+    input: Storage,
     input_shape: Shape,
     input_strides: Strides,
-    weight: Tensor,
+    weight: Storage,
     weight_shape: Shape,
     weight_strides: Strides,
     reverse: bool,
@@ -135,7 +131,7 @@ def _tensor_conv1d(
         out[index_to_position(out_index, out_strides)] = val
 
 
-tensor_conv1d = njit(parallel=True)(_tensor_conv1d)
+tensor_conv1d = njit(_tensor_conv1d, parallel=True)
 
 
 class Conv1dFun(Function):
@@ -174,23 +170,23 @@ class Conv1dFun(Function):
         grad_weight = grad_output.zeros((in_channels, out_channels, kw))
         new_input = input.permute(1, 0, 2)
         new_grad_output = grad_output.permute(1, 0, 2)
-        tensor_conv1d(
+        tensor_conv1d(  # type: ignore
             *grad_weight.tuple(),
             grad_weight.size,
             *new_input.tuple(),
             *new_grad_output.tuple(),
-            False,
+            False,  # type: ignore
         )
         grad_weight = grad_weight.permute(1, 0, 2)
 
         grad_input = input.zeros((batch, in_channels, w))
         new_weight = weight.permute(1, 0, 2)
-        tensor_conv1d(
+        tensor_conv1d(  # type: ignore
             *grad_input.tuple(),
-            grad_input.size,
+            grad_input.size,  # type: ignore
             *grad_output.tuple(),
             *new_weight.tuple(),
-            True,
+            True,  # type: ignore
         )
         return grad_input, grad_weight
 
@@ -199,14 +195,14 @@ conv1d = Conv1dFun.apply
 
 
 def _tensor_conv2d(
-    out: Tensor,
+    out: Storage,
     out_shape: Shape,
     out_strides: Strides,
     out_size: int,
-    input: Tensor,
+    input: Storage,
     input_shape: Shape,
     input_strides: Strides,
-    weight: Tensor,
+    weight: Storage,
     weight_shape: Shape,
     weight_strides: Strides,
     reverse: bool,
@@ -256,6 +252,9 @@ def _tensor_conv2d(
 
     s1 = input_strides
     s2 = weight_strides
+    # inners
+    s10, s11, s12, s13 = s1[0], s1[1], s1[2], s1[3]  # noqa: F841
+    s20, s21, s22, s23 = s2[0], s2[1], s2[2], s2[3]  # noqa: F841
 
     # TODO: Implement for Task 4.2.
     for p in prange(out_size):
@@ -304,7 +303,7 @@ def _tensor_conv2d(
         out[index_to_position(out_index, out_strides)] = val
 
 
-tensor_conv2d = njit(parallel=True, fastmath=True)(_tensor_conv2d)
+tensor_conv2d = njit(_tensor_conv2d, parallel=True, fastmath=True)
 
 
 class Conv2dFun(Function):
@@ -342,27 +341,25 @@ class Conv2dFun(Function):
         grad_weight = grad_output.zeros((in_channels, out_channels, kh, kw))
         new_input = input.permute(1, 0, 2, 3)
         new_grad_output = grad_output.permute(1, 0, 2, 3)
-        tensor_conv2d(
+        tensor_conv2d(  # type: ignore
             *grad_weight.tuple(),
             grad_weight.size,
             *new_input.tuple(),
             *new_grad_output.tuple(),
-            False,
+            False,  # type: ignore
         )
         grad_weight = grad_weight.permute(1, 0, 2, 3)
 
         grad_input = input.zeros((batch, in_channels, h, w))
         new_weight = weight.permute(1, 0, 2, 3)
-        tensor_conv2d(
+        tensor_conv2d(  # type: ignore
             *grad_input.tuple(),
-            grad_input.size,
+            grad_input.size,  # type: ignore
             *grad_output.tuple(),
             *new_weight.tuple(),
-            True,
+            True,  # type: ignore
         )
         return grad_input, grad_weight
 
 
 conv2d = Conv2dFun.apply
-
-# style error
